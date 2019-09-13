32a2b5a3a66f7d5030a203ea8379396f7c98d9bc
Ankur <ankurpohekar286@gmail.com>
2019-09-13 12:28:24 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+	var btn = document.createElement("BUTTON");
+	btn.innerHTML = "CLICK";
+	document.body.appendChild(btn);
+	btn.style.cssText="top: 50px;width: 60px;right: 10px;border-radius: 19px;";
+}
